7ddfb3a724fd9aa6915f34dedc9703da71fb0ea5
brrond <danya17072@gmail.com>
2025-12-01 21:31:51 +0100

    WIP: Add simple one-line "sudoku" with free vars

---
+++ muli-env/muli-runtime/src/test/java/brrond/sudoku/SudokuTest.java
@@ -0,0 +1,18 @@
+package brrond.sudoku;
+
+import de.wwu.muggl.configuration.Options;
+import de.wwu.muggl.vm.classfile.ClassFileException;
+import de.wwu.muggl.vm.execution.nativeWrapping.TestablePrintStreamWrapper;
+import de.wwu.muli.env.TestableMuliRunner;
+import de.wwu.muli.searchtree.ST;
+import org.junit.Test;
+
+public class SudokuTest {
+
+    @Test
+    public final void testGetSolutions() throws InterruptedException, ClassFileException {
+        Options.getInst().actualCliPrinting = true;
+        ST[] foundTrees = TestableMuliRunner.runApplication("brrond.sudoku.Sudoku");
+        TestablePrintStreamWrapper.outputStream().resetBuffer(); // To reset output stream
+    }
+}
